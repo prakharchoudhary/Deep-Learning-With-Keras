94f064c58f08bf2829e3b42aa7ff7e09c6234d01
prakharchoudhary <prakhar2397@gmail.com>
2017-11-11 01:53:26 +0530

    minor changes fixed

--- ch03/cifar-cnn.py
+++ ch03/cifar-cnn.py
@@ -29,8 +29,8 @@ print(X_train.shape[0], 'training samples')
 print(X_test.shape[0], 'testing samples')
 
 # convert to categorical
-y_train = np_utils.to_categorical(X_train)
-y_test = np_utils.to_categorical(X_test)
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
 
 # float and normalization
 X_train = X_train.astype('float32')
@@ -41,12 +41,12 @@ X_test /= 255
 # model
 model = Sequential()
 model.add(Conv2D(32, (3, 3), padding='same',
-                 input_shape = (IMG_ROWS, IMG_COLS, IMG_CHANNELS)))
+                 input_shape=(IMG_ROWS, IMG_COLS, IMG_CHANNELS)))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size(2, 2)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(32))
+model.add(Dense(512))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(NB_CLASSES))
