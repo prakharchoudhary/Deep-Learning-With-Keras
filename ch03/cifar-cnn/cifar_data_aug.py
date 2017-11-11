7d631bfcda0ce93c0f272323cb5a54bae7a5a3fc
prakharchoudhary <prakhar2397@gmail.com>
2017-11-12 03:57:31 +0530

    dcnn on augmented data

---
+++ ch03/cifar-cnn/cifar_data_aug.py
@@ -0,0 +1,125 @@
+import os
+from keras.datasets import cifar10
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.optimizers import SGD, Adam, RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+import numpy as np
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# CIFAR_10 is a set of 60K images 32x32 pixels on 3 channels
+IMG_CHANNELS = 3
+IMG_ROWS = 32
+IMG_COLS = 32
+
+# constant
+BATCH_SIZE = 128
+NB_EPOCH = 50
+NB_CLASSES = 10
+VERBOSE = 1
+VALIDATION_SPLIT = 0.2
+OPTIM = RMSprop()
+
+# load dataset
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'training samples')
+print(X_test.shape[0], 'testing samples')
+
+
+# convert to categorical
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+# float and normalization
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+# model
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=(IMG_ROWS, IMG_COLS, IMG_CHANNELS)))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Conv2D(64, (3, 3), padding='same',
+                 input_shape=(IMG_ROWS, IMG_COLS, IMG_CHANNELS)))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(NB_CLASSES))
+model.add(Activation('softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer=OPTIM,
+              metrics=['accuracy'])
+# augmenting
+print("Augmenting training set images...")
+datagen = ImageDataGenerator(
+		featurewise_center=False,
+        samplewise_center=False,
+        featurewise_std_normalization=False,
+        samplewise_std_normalization=False,
+		zca_whitening=False,
+		rotation_range=40,
+		width_shift_range=0.2,
+		height_shift_range=0.2,
+		zoom_range=0.2,
+		horizontal_flip=True,
+		fill_mode='nearest')
+
+datagen.fit(X_train)
+
+
+# train
+history = model.fit(X_train, y_train, batch_size=BATCH_SIZE,
+	epochs=NB_EPOCH, validation_split=VALIDATION_SPLIT, 
+	verbose=VERBOSE)
+
+#model.fit_generator(datagen.flow(X_train, Y_train,
+#                        batch_size=BATCH_SIZE),
+#                        samples_per_epoch=X_train.shape[0],
+#                        nb_epoch=NB_EPOCH, 
+#                        verbose=VERBOSE)
+score = model.evaluate(X_test, y_test, verbose=VERBOSE)
+print("Test Score:", score[0])
+print("Test accuracy:", score[1])
+
+# save the architecture
+model_json = model.to_json()
+open('cifar10_architecture.json', 'w').write(model_json)
+model.save_weights('cifar10_weights.h5', overwrite=True)
+
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+plt.gcf.clear()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
